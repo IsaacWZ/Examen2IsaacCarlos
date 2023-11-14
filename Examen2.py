5d5111cf51e927447fbd95c840b61c0d3de1b20d
IsaacWZ <jocorrales@estudiantec.cr>
2023-11-13 19:01:13 -0600

    Que se vea bonito

--- Examen2.py
+++ Examen2.py
@@ -45,46 +45,30 @@ class TestMiClase(unittest.TestCase):
 
         self.assertEqual(mi_clase.ObtieneValencia(13579), 5)
         self.assertEqual(mi_clase.ObtieneValencia(11111), 5)
-
-
-
-
-
     def test_DivisibleTempo(self):
         mi_clase = MiClase(None, None, None, None, None)
         #Isaac
         self.assertEqual(mi_clase.DivisibleTempo(10), [1, 2, 5, 10])
         self.assertEqual(mi_clase.DivisibleTempo(7), [1, 7])
-
         #Carlos
         self.assertEqual(mi_clase.DivisibleTempo(15), [1, 3, 5, 15])
         self.assertEqual(mi_clase.DivisibleTempo(20), [1, 2, 4, 5, 10, 20])
-
     def test_ObtieneMasBailable(self):
         mi_clase = MiClase(None, None, None, None, None)
         #Isaac
         mi_clase = MiClase(None, None, None, None, None)
         self.assertEqual(mi_clase.ObtieneMasBailable([0.8, 0.9, 0.7]), 0.9)
         self.assertIsNone(mi_clase.ObtieneMasBailable([]))
-
         #Carlos
         self.assertEqual(mi_clase.ObtieneMasBailable([1, 5, 3, 7, 2]), 7)
         self.assertEqual(mi_clase.ObtieneMasBailable([0, 10, 15, 5]), 15)
-
-
-
     def test_VerificaListaCanciones(self):
         mi_clase = MiClase(None, None, None, None, None)
         #Isaac
         self.assertTrue(mi_clase.VerificaListaCanciones([1, 2, 3, 4, 5]))
         self.assertFalse(mi_clase.VerificaListaCanciones([1, None, 3, 4, 5]))
-
-
         #Carlos
         self.assertFalse(mi_clase.VerificaListaCanciones([None, None, None]))
         self.assertTrue(mi_clase.VerificaListaCanciones([1, 2, 3, 4, 5, "song"]))
-
-
-
 if __name__ == '__main__':
     unittest.main()
